6eaf416d05c5c3914abb11584f5b3696df7718b8
Maxime FRANC <mfranc@e2r3p2.42.fr>
2017-01-19 20:32:14 +0100

    more test on printf during read the man

--- main_printf.c
+++ main_printf.c
@@ -6,11 +6,12 @@
 /*   By: mfranc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/18 12:26:11 by mfranc            #+#    #+#             */
-/*   Updated: 2017/01/18 21:31:44 by mfranc           ###   ########.fr       */
+/*   Updated: 2017/01/19 20:31:18 by mfranc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include "libft.h"
 #include <stdio.h>
 
 int	main(void)
@@ -30,5 +31,43 @@ int	main(void)
 //	printf("---%*d----\n", 6, number);
 //	printf("%#o\n", 34);
 //	printf("%u", 543);
-	ft_printf("BONJOUR");
+	printf("BONJOUR\n\n");
+	printf("Test du flag # avec l'option g avec float |%#g|\n\n", 677.65);
+	printf("TEST PRECISION : \n");
+	printf("Test de padding a gacuhe |%50s|\n", "bonjour c'est maxime'");
+	printf("Test de padding a gauche avec un int |%50x|\n", 7753);
+	printf("\n");
+	printf("Test de padding inferieure a loption s |%10s|\n", "bonjour c'est maxime'");
+	printf("Test de padding inferieure a loption lx |%10lx|\n", 775539087676);
+	printf("\n");
+	printf("Test de padding a droite avec le flag - avant le padding |%-10s|\n", "s maxime'");
+	printf("Test de padding a droite avec le flag - avant le padding |%-10x|\n", 7753);
+	printf("\n");
+	printf("Test de padding a droite avec le falg - apres le padding |%50-s|\n", "bonjour c'est maxime'");
+	printf("Test de padding a droite avec le flag - apres le padding |%50-x|\n", 7753);	
+	printf("\n");
+	printf("Test de 0 padding avec la flag - derriere le padding |%0-s |\n ", "bonjour c'est maxime'");
+	printf("Test de 0 padding avec le fflag - derriere  le padding |%0-x|\n", 7753);
+	printf("Test de 0 padding |%0s|\n", "bonjour c'est maxime'");
+	printf("Test de 0 padding |%0x|\n", 7753);
+	printf("Test du - tout seul |%-0s|\n", "bonjour c'est maxime'");
+	printf("Test du - tout seul |%-0x|\n", 7753);
+	printf("\n");
+	printf("Test dun padding a gauche puis tester la precision avec un char * |%30.10s|\n", "bonjour c'est maxime'");
+	printf("Test dun padding a gauche puis tester la precision avec un int |%30.7d|\n", 7753);
+	printf("Test de padding - avec un plus pour voir ce que ca fait |%-45s|\n", "bonjour sa va");
+	printf("Test de padding - avec un plus pour voir ce que ca fait |%-+45lld|\n", 843256743567890);
+	printf("\n");
+	printf("Test du blank avnt un int |% d\n|", 43);
+	printf("Test du blank avnt un long int |% d\n|", 43);
+	printf("Test du blank avnt un long long int |% d\n|", 43);
+	printf("Test du blank avnt un int negative |% d\n|", -43);
+	printf("Test du blank avnt un unsigned int |% u\n|", 4243563);
+	printf("Test du blank avnt un string |% s\n|", "bkn");
+	printf("\n");
+	printf("test du flag + avec un signed int |%   #  o\n", 645);
+	printf("test du flag + avec un signed neg int |%   #   x\n", -645);
+	printf("test du flag + avec un signed long long int |%.10 lld\n", 628967);
+	printf("test du flag + avec un signed long long int |%4lld\n", 4254);
+	printf("test du flag + avec un signed unsigned int |%     u\n", 6234567896543245);
 }
