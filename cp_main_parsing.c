b8e595fafc5d741f17ef06859904d84632d3a0d1
Maxime FRANC <mfranc@e2r4p18.42.fr>
2017-02-09 18:11:07 +0100

    saving

--- cp_main_parsing.c
+++ cp_main_parsing.c
@@ -6,7 +6,7 @@
 /*   By: mfranc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/05 18:01:51 by mfranc            #+#    #+#             */
-/*   Updated: 2017/02/08 22:00:01 by mfranc           ###   ########.fr       */
+/*   Updated: 2017/02/09 11:43:20 by mfranc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ int	main(void)
 //	printf("%d\n", printf("okcestcool"));
 //	printf("%d\n", printf("bgfko%"));
 //	printf("%d\n", printf("Bonjour %s yeee on est bien", "maxime"));
-//	printf("%d\n", printf("Yo, %789)*&^@#s et son ami %)(*)(&&*()))s sont de tres bons amis, ils sont a %ZZZZZ$@s 42", "Maxime", "COUCOUUUUUU", "lecole"));
-//	printf("%d\n", printf("Yo, %s et son ami %s sont de tres bons amis, ils sont a %s 42", "Maxime", "COUCOUUUUUU", "lecole"));
+//	printf("%d\n", printf("Yo, %789)*&^@# et son ami %)(*)(&&*())) sont de tres bons amis, ils sont a %ZZZZZ$@ 42", "Maxime", "COUCOUUUUUU", "lecole"));
+	printf("%d\n", printf("Yo, %@!#$&*() et son ami %&*()) sont de tres bons amis, ils sont a %$&*^()) 42", "Maxime", "Aymeric", "lecole"));
 //	printf("%d\n", printf("Yo, %s et son ami %s sont de tres bons amis", "Maxime", "COUCOUUUUUU"));
 	return (0);
 }
--- main_parsing.c
+++ main_parsing.c
@@ -6,7 +6,7 @@
 /*   By: mfranc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/05 18:01:51 by mfranc            #+#    #+#             */
-/*   Updated: 2017/02/08 22:00:01 by mfranc           ###   ########.fr       */
+/*   Updated: 2017/02/09 11:47:50 by mfranc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@
 
 int	main(void)
 {
-//	printf("%d\n", ft_printf(""));
-//	printf("%d\n", ft_printf("okcestcool"));
-//	printf("%d\n", ft_printf("bgfko%"));
-//	printf("%d\n", ft_printf("Bonjour %s yeee on est bien", "maxime"));
-//	printf("%d\n", ft_printf("Yo, %789)*&^@#s et son ami %)(*)(&&*()))s sont de tres bons amis, ils sont a %ZZZZZ$@s 42", "Maxime", "COUCOUUUUUU", "lecole"));
-//	printf("%d\n", ft_printf("Yo, %s et son ami %s sont de tres bons amis, ils sont a %s 42", "Maxime", "COUCOUUUUUU", "lecole"));
-//	printf("%d\n", ft_printf("Yo, %s et son ami %s sont de tres bons amis", "Maxime", "COUCOUUUUUU"));
+	printf("%d\n", ft_printf(""));
+	printf("%d\n", ft_printf("okcestcool"));
+	printf("%d\n", ft_printf("bgfko%"));
+	printf("%d\n", ft_printf("Bonjour %s yeee on est bien", "maxime"));
+	printf("%d\n", ft_printf("Yo, %@!#$&*() et son ami %&*()) sont de tres bons amis, ils sont a %$&*^()) 42", "Maxime", "Aymeric", "lecole"));
+	printf("%d\n", ft_printf("Yo, %@!#$&*()s et son ami %s sont de tres bons amis, ils sont a %$&*^())s 42", "Maxime", "Aymeric", "lecole"));
+	printf("%d\n", ft_printf("Yo, %s et son ami %s sont de tres bons amis", "Maxime", "COUCOUUUUUU"));
 	return (0);
 }
